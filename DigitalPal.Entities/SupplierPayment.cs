67af87e1b29083d6fb21920c0f6e907217a9883f
spatil <spatil@L005665>
2018-11-22 12:24:39 +0530

    Removed Invoice Referabce from Supplier Payment

--- DigitalPal.Entities/SupplierPayment.cs
+++ DigitalPal.Entities/SupplierPayment.cs
@@ -6,8 +6,6 @@ namespace DigitalPal.Entities
     public class SupplierPayment: BaseEntity
     {
         public DateTime PaymentDate { get; set; }
-        public string InvoiceNumber { get; set; }
-        public Guid? InvoiceId { get; set; }
         public string SupplierOrderNumber { get; set; }
         public Guid? SupplierOrderId { get; set; }
         public string SupplierName { get; set; }
